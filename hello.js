cb0bdc6d99cb57b2f05fc815cccfc6b3e791dfc4
Jose de la Puente <14jdelap@gmail.com>
2020-07-03 14:26:11 +0200

    Add basic index with an alert of the current weekday and a greeting in the body with the weekday. Also add a separate hello JS file (without extension) to execute via shell — good for quick checks.

---
+++ hello.js
@@ -0,0 +1,9 @@
+console.log("Trying this out!",
+"hope it works!");
+alert("hello, world!");
+alert("this is the second alert!");
+
+let firstName = 'Jose';
+let lastName = 'de la Puente';
+
+console.log("${firstName} is my name");
